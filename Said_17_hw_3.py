7f0c88403a3f16cb77b52959a127bb6c671b9837
Rhead04 <103441139+Rhead04@users.noreply.github.com>
2022-04-15 14:38:37 +0600

    Add files via upload

---
+++ Said_17_hw_3.py
@@ -0,0 +1,50 @@
+"""
+HW
+"""
+# class Fraction():
+#     def __init__(self, numerator, denominator):
+#         self.numerator = numerator
+#         self.denominator = denominator
+#
+#     def _get_num(self):
+#         print(f"{self.numerator} / {self.denominator}")
+#
+#     def __add__(self, other):
+#         self._get_num()
+#         self.numerator += other
+#         self.denominator += other
+#         self._get_num()
+#
+#     def __sub__(self, other):
+#         self._get_num()
+#         self.numerator -= other
+#         self.denominator -= other
+#         self._get_num()
+#
+#     def __mul__(self, other):
+#         self._get_num()
+#         self.numerator *= other
+#         self.denominator *= other
+#         self._get_num()
+#
+#     def __floordiv__(self, other):
+#         self._get_num()
+#         self.numerator //= other
+#         self.denominator //= other
+#         self._get_num()
+#
+#
+# fraction = Fraction(5,10)
+
+"""
+EXTRA TASK
+"""
+#
+# x = input('Enter the numbers: ')
+# x = str(x)
+# copy_x = x[::-1]
+#
+# if x == copy_x:
+#     print(True)
+# else:
+#     print(False)
